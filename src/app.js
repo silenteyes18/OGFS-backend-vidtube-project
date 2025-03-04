2f6982eb524818a54b22225717f27f06a6c8ac7a
silenteyes18 <mohanlokesh1999@gmail.com>
2025-03-05 02:14:19 +0530

    add the code of mongoDB connection and standadized way of handling the server request and response with healthCheck request

--- src/app.js
+++ src/app.js
@@ -12,4 +12,10 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 
+// Import routes
+import healthchekRouter from "./routes/healthCheck.routs.js";
+
+// Router setup
+app.use("/api/v1/healthcheck", healthchekRouter);
+
 export {app};
--- src/db/index.js
+++ src/db/index.js
@@ -3,10 +3,10 @@ import {DB_NAME} from "../constants.js";
 
 const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}?retryWrites=true&w=majority`);
-        console.log(`MongoDB connected! ${connectionInstance}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        console.log(`MongoDB connected! <3 ${connectionInstance}`);
         console.log(`Host: ${connectionInstance.connection.host}`);
-        
+        // console.log(connectionInstance);
     } catch (error) {
         console.log("MongoDB Connection Error", error);
         process.exit(1);// to exit after the error
